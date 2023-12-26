81b2d089c4422248b741ba38141db0e6896cf57e
s12f <siwen.feng@emqx.io>
2023-12-26 12:32:06 +0800

    feat(base): add datagen

---
+++ base/install-connector.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Invalid Arguements, e.g.: ./install-connectors.sh <TARGET_DIR> <CONNECTOR_URL>"
+    exit 1
+fi
+
+set -x
+
+TARGET_DIR=$1
+CONNECTOR_URL=$2
+
+cd $TARGET_DIR
+
+if [[ $CONNECTOR_URL == *.tar.gz ]]; then
+    curl -s -L $CONNECTOR_URL | tar -zxf - -C .
+elif [[ $CONNECTOR_URL == *.zip ]]; then
+    TMP_FILE=$(mktemp --suffix=.zip) && \
+    curl -s -L $CONNECTOR_URL -o $TMP_FILE && unzip $TMP_FILE && \
+    rm $TMP_FILE
+else
+    echo "unsupported url:${CONNECTOR_URL}"
+    echo "plugin file must be *.tar.gz, *.zip"
+    exit 1
+fi
