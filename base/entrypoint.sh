8272a2f878fd2f377cc03721681ab48b4fd47725
s12f <siwen.feng@emqx.io>
2023-12-26 11:08:33 +0800

    init

---
+++ base/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mode=${CONNECT_MOD:-standalone}
+
+/opt/envsubst \
+    -i /etc/kafka/connect-${mode}.properties.tmpl \
+    -o /etc/kafka/connect.properties
+
+cp /opt/kafka/bin/connect-${mode}.sh /opt/kafka/bin/connect.sh
+
+exec "$@"
